4384dfa00f348bcdd261f06cfb9363a171b2ddad
Claudio <claudiof@gmail.com>
2019-03-28 15:00:52 -0300

    Add a way  to rewrite array indexes to business keys

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { curry, over, lensProp, pipe, sortBy, prop, map, groupWith, equals, head 
 const transform = require("@bluealba/object-transform");
 const calculateDiff = require("deep-diff");
 const chalk = require("chalk");
+const { lensPath, view } = require("ramda");
 
 const reconciliate = curry((rules, before, after) => {
 	const beforeNormalized = normalizeBefore(rules)(before);
@@ -14,23 +15,36 @@ const reconciliate = curry((rules, before, after) => {
 		sortBy(prop("path"))
 	)(result);
 
-	return new DiffResult(diff);
+	return new DiffResult(diff, before, after);
 })
 
 class DiffResult {
-	constructor(diff) {
+	constructor(diff, before, after, displayPathReplacers = []) {
 		this.diff = diff;
+		this.before = before;
+		this.after = after;
+		this.displayPathReplacers = displayPathReplacers;
+	}
+	
+	parseExpression(expression) {
+		//TODO: WIP, not sure about the notation. It only needs to replace captured groups!
+		return expression
+			.replace(/\./g, "\\.") //dots are textual dots
+			.replace(/\[\]/g, "\\d+") //square brackets match any index in an array, this won't work!
+			.replace(/\?/g, "[\\w\\d]+") //quotation mark matches any key
+	}
+	
+	getValue(path) {
+		const splitPath = path.split(".").map(x => x.match(/^\d+$/) ? parseInt(x) : x);
+		return view(lensPath(splitPath))(this.before);
 	}
 
 	/**
 	 * Consolidate all the differences that match a given expression into a single occurrence
 	 */
 	consolidate(expression) {
-		const regexp = new RegExp("^" + expression
-			.replace(/\./g, "\\.") //dots are textual dots
-			.replace(/\[\]/g, "(\\d+)") //square brackets match any index in an array, it creates a capturing group
-			.replace(/\*/g, "([\\w\\d\\.]+)")); //star match any piece of path, it creates a capturing group
-
+		const regexp = new RegExp(`^${this.parseExpression(expression)}`); 
+		
 		const consolidatedDiff = pipe(
 			map(each => Object.assign({}, each, { groupPath: each.path.replace(regexp, expression) })),
 			sortBy(each => `${each.groupPath} ${JSON.stringify(each.rhs)} ${JSON.stringify(each.lhs)}`),
@@ -47,13 +61,31 @@ class DiffResult {
 			}))
 		)(this.diff)
 
-		return new DiffResult(consolidatedDiff)
+		return new DiffResult(consolidatedDiff, this.before, this.after, this.displayPathReplacers)
+	}
+	
+	displayKey(expression, keyExtractor) {
+		const regexp = new RegExp(`^(${this.parseExpression(expression)})\\.([\\d\\w]+)(.*)$`); 
+		
+		const displayPathReplacer = path => {
+			if (path.match(regexp)) {
+				const keyHolderPath = path.replace(regexp, `$1.$2`);
+				const keyHolder = this.getValue(keyHolderPath);
+				const extractedKey = keyExtractor(keyHolder);
+				return path.replace(regexp, `$1.${extractedKey}$3`)
+			} else {
+				return path;
+			}
+		}
+		
+		return new DiffResult(this.diff, this.before, this.after, [...this.displayPathReplacers, displayPathReplacer])
 	}
 
 	printDiff() {
 		return pipe(
 			map(each => {
-				let line = `${chalk.yellow(each.path)} -> ${chalk.grey(JSON.stringify(each.lhs))} ${chalk.white(JSON.stringify(each.rhs))}`
+				let path = this.displayPathReplacers.reduce((path, replacer) => replacer(path), each.path);
+				let line = `${chalk.yellow(path)} -> ${chalk.grey(JSON.stringify(each.lhs))} ${chalk.white(JSON.stringify(each.rhs))}`
 				if (each.times && each.times.length > 1) {
 					line = `${line} ${chalk.yellow(`(${each.times.length} times)`)}`
 				}
